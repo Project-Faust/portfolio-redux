7909031eaa695402c3beea025604698f8f18e925
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-12 22:18:18 -0400

    updated functionality to map projects from data.js and render appropriate data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,1 +1,32 @@
-export default function Projects() {};
+import React, { useState } from "react";
+import { projects } from "../data";
+
+export default function Projects() {
+    const project = useState(projects);
+    return (
+        <div>
+            {project.map((res) => (
+                <div className="project">
+                    <div className="projectText">
+                        <a href={res.deployed} target="_blank" rel="noreferrer">
+                            <h2>{res.title}</h2>
+                        </a>
+                        <p className="projectDescription">{res.description}
+                            <a className="githubLink" href={res.repository} target="_blank">Repository</a>
+                        </p>
+                    </div>
+                    <a className="projectImageAnchor"
+                        href={res.deployed}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img
+                            src={res.image}
+                            alt="preview"
+                            className="projectImage" />
+                    </a>
+                </div>
+            ))}
+        </div>
+    )
+};
