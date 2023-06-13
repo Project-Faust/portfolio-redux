b91d6b58f1b9dc376b1f6baf91e8924fd227cd58
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-12 20:21:03 -0400

    added nav bar styling, content, hrefs, default active (about)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,20 @@
-export default function Navbar() {};
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <ul class="nav nav-tabs justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#projects">Projects</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#skills">Skills</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contac">Contact</a>
+            </li>
+        </ul>
+    )
+};
