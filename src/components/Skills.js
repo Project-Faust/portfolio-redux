bbe4b34e61ebf483646837d6fd73529f0b2c4544
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-13 18:42:34 -0400

    added react functionality to display information from data.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,1 +1,20 @@
-export default function Skills() {};
+import React from "react";
+import { skills } from "../data";
+
+export default function Skills() {
+    return (
+        <section>
+            <div className="skillHeader text-center m-2">
+                <h1>Skills</h1>
+                <p>Here are a list of skills and other technologies that I have profiency with.</p>
+            </div>
+            <div className="skillContainer text-center">
+                {skills.map((res) => (
+                    <div key={res.name} className="mb-3">
+                        <span><strong>{res.name}</strong></span>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
+};
