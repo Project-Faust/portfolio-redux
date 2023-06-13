e832e478c513b7e8a546eb71c9d365d2ad1e7675
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-12 23:01:40 -0400

    updated file path for image

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
         title: 'API Playground',
         subtitle: '',
         description: 'Sandbox Learning Tool for Web APIs',
-        image: '',
+        image: require('./assets/images/api-playground.png'),
         deployed: 'https://arcane-reaches-06891.herokuapp.com/',
         repository: 'https://github.com/Project-Faust/API-Playground',
     }
@@ -14,6 +14,7 @@ export const skills = [
         name: ''
     },
 ]
+
 
 // project template
 // {
