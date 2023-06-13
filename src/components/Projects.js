efcf8b551615fc0890a906d3bc2eadd1bfcc9efd
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-12 22:26:20 -0400

    added functionality to map project data from file; updated DOM structure

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,30 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import { projects } from "../data";
 
 export default function Projects() {
-    const project = useState(projects);
     return (
         <div>
-            {project.map((res) => (
-                <div className="project">
+            {projects.map((res) => (
+                <div className="projectContainer text-center m-2">
                     <div className="projectText">
                         <a href={res.deployed} target="_blank" rel="noreferrer">
                             <h2>{res.title}</h2>
+                            <a className="projectImageAnchor"
+                                href={res.deployed}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <img
+                                    src={res.image}
+                                    alt="preview"
+                                    className="projectImage" />
+                            </a>
                         </a>
                         <p className="projectDescription">{res.description}
-                            <a className="githubLink" href={res.repository} target="_blank">Repository</a>
+                            <br />
+                            <a className="githubLink" href={res.repository}
+                                target="_blank"
+                                rel="noreferrer">Repository</a>
                         </p>
                     </div>
-                    <a className="projectImageAnchor"
-                        href={res.deployed}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <img
-                            src={res.image}
-                            alt="preview"
-                            className="projectImage" />
-                    </a>
                 </div>
             ))}
         </div>
