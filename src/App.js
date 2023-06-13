e73ed7e219d682526c9d25c7ab4c2f9406c54fef
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-12 21:58:50 -0400

    updated to dynamically render based off current nav link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import './App.css';
 
 export default function App() {
+  const [navLinks] = useState([
+    { name: "About" },
+    { name: "Projects" },
+    { name: "Skills" },
+    { name: "Contact" }
+  ]);
+
+  const [currentNav, setCurrentNav] = useState(navLinks[0]);
+
   return (
-    <main>
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-    </main>
+    <div>
+      <header>
+        <Navbar
+          navLinks={navLinks}
+          currentLink={currentNav}
+          setCurrentLink={setCurrentNav}
+        />
+      </header>
+      <main>
+        {currentNav === navLinks[0] && <About></About>}
+        {currentNav === navLinks[1] && <Projects></Projects>}
+        {currentNav === navLinks[2] && <Skills></Skills>}
+        {currentNav === navLinks[3] && <Contact></Contact>}
+      </main>
+    </div>
   );
 };
