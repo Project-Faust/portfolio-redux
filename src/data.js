758d61ac6c98276dd7ef2c5a03f4566818359fd7
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-13 18:42:03 -0400

    added additional content to projects and skills

--- src/data.js
+++ src/data.js
@@ -6,12 +6,76 @@ export const projects = [
         image: require('./assets/images/api-playground.png'),
         deployed: 'https://arcane-reaches-06891.herokuapp.com/',
         repository: 'https://github.com/Project-Faust/API-Playground',
-    }
+    },
+    {
+        title: 'Belmont Drive-In Redux',
+        subtitle: '',
+        description: 'Business Page for the Belmont Drive-In Theatre',
+        image: require('./assets/images/drive-in-redux-logo.png'),
+        deployed: 'https://project-faust.github.io/drive-in-redux/',
+        repository: 'https://github.com/Project-Faust/drive-in-redux',
+    },
+    {
+        title: '',
+        subtitle: '',
+        description: '',
+        image: '',
+        deployed: '',
+        repository: '',
+    },
+    {
+        title: '',
+        subtitle: '',
+        description: '',
+        image: '',
+        deployed: '',
+        repository: '',
+    },
+    {
+        title: '',
+        subtitle: '',
+        description: '',
+        image: '',
+        deployed: '',
+        repository: '',
+    },
+    {
+        title: '',
+        subtitle: '',
+        description: '',
+        image: '',
+        deployed: '',
+        repository: '',
+    },
 ]
 
 export const skills = [
     {
-        name: ''
+        name: 'HTML5'
+    },
+    {
+        name: 'CSS'
+    },
+    {
+        name: 'JavaScript'
+    },
+    {
+        name: 'Git'
+    },
+    {
+        name: 'MySQL'
+    },
+    {
+        name: 'MongoDB'
+    },
+    {
+        name: 'Express.js'
+    },
+    {
+        name: 'React.js'
+    },
+    {
+        name: 'Node.js'
     },
 ]
 
