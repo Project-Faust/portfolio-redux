46ec4d21e0ed49b76c588093d602fe6ce1fb315b
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-12 22:01:31 -0400

    created base template for projects/skills

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const projects = [
+    {
+        title: '',
+        subtitle: '',
+        description: '',
+        image: '',
+        link: ''
+    }
+]
+
+export const skills = [
+    {
+        name: ''
+    },
+]
+
+// project template
+// {
+//     title: '',
+//     subtitle: '',
+//     description: '',
+//     image: '',
+//     link: ''
+// },
+
+// skill template
+// {
+//     name: ''
+// },
