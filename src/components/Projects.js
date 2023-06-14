c95399297f089adfc1d87bf8fccd952eb9d740fc
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-13 22:04:25 -0400

    updated styling to make more symmetrical and easy to read

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,37 +2,54 @@ import React from "react";
 import { projects } from "../data";
 
 export default function Projects() {
-    return (
-        <section>
-            <div className="projectsHeader text-center m-2">
-                <h1>Projects</h1>
-                <p>Listed below are some projects I have created or contributed to.</p>
+  return (
+    <section className="m-3">
+      <div className="projectsHeader text-center">
+        <h1>Projects</h1>
+        <p>Listed below are some applications I have created or contributed to.</p>
+      </div>
+      {projects.map((res) => (
+        <div className="row projectContainer border-top">
+          <div className="col-md-6 d-flex align-items-center justify-content-center text-center">
+            <div className="projectText">
+              <a
+                className="text-dark text-decoration-none"
+                href={res.deployed}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <h2>{res.title}</h2>
+                <p>({res.subtitle})</p>
+              </a>
+              <p className="projectDescription">
+                {res.description}
+                </p>
+                <a
+                  className="githubLink text-dark text-decoration-none"
+                  href={res.repository}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <strong>GitHub Repository</strong>
+                </a>
             </div>
-            {projects.map((res) => (
-                <div className="projectContainer text-center m-2 border-top">
-                    <div className="projectText">
-                        <a className="text-decoration-none" href={res.deployed} target="_blank" rel="noreferrer">
-                            <h2 className="">{res.title}</h2>
-                            <a className="projectImageAnchor"
-                                href={res.deployed}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <img
-                                    src={res.image}
-                                    alt="preview"
-                                    className="projectImage img-fluid" />
-                            </a>
-                        </a>
-                        <p className="projectDescription">{res.description}
-                            <br />
-                            <a className="githubLink text-decoration-none" href={res.repository}
-                                target="_blank"
-                                rel="noreferrer">GitHub Repository</a>
-                        </p>
-                    </div>
-                </div>
-            ))}
-        </section>
-    )
-};
+          </div>
+          <div className="col-md-6 d-flex align-items-center justify-content-center">
+            <a
+              className="projectImageAnchor"
+              href={res.deployed}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={res.image}
+                alt="preview"
+                className="projectImage img-fluid m-3"
+              />
+            </a>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+}
