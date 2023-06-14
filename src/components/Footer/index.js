b56cdec6e2514fb0bafb62ccd6eaf05a2ce44006
Lane Pentecost <pentecost.cody.lane@gmail.com>
2023-06-14 19:33:10 -0400

    updated content; added styling; imported css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,37 @@
 import React from "react";
+import "./index.css"
+import GitHub from "../../assets/images/github-mark.png";
+import LinkedIn from "../../assets/images/LI-In-Bug.png";
 
-export default function Footer() {};
+export default function Footer() {
+    return (
+        <footer
+            id="footer"
+            className="d-flex border-top align-items-center justify-content-center bg-light mt-3">
+            <a
+                className="mx-3 my-1"
+                href="https://github.com/Project_faust"
+            >
+                <img
+                    src={GitHub}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="footerImage"
+                    alt="GitHub"
+                ></img>
+            </a>
+            <a
+                className="mx-3 my-1"
+                href="https://www.linkedin.com/in/richie-lollis-042bb0235/"
+            >
+                <img
+                    src={LinkedIn}
+                    target="_blank"
+                    rel="norefferer"
+                    alt="LinkedIn"
+                    className="footerImage"
+                />
+            </a>
+        </footer>
+    );
+}
