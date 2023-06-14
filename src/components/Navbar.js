88070b8842a9929d827d250e714ff7f9b9a69380
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-13 22:03:31 -0400

    updated text styling and background for sticky navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Navbar({ navLinks = [], currentLink, setCurrentLink }) {
     return (
-        <ul className="nav nav-tabs justify-content-center">
+        <ul className="nav nav-tabs sticky-top justify-content-center bg-light">
             {navLinks.map((navLink) => (
                 <li key={navLink.name} className="nav-item">
                     <a
-                        className={`nav-link ${currentLink === navLink ? 'active' : ''}`}
+                        className={`nav-link text-dark ${currentLink === navLink ? 'active' : ''}`}
                         href={navLink.url}
                         onClick={() => {
                             setCurrentLink(navLink);
