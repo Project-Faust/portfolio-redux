e1008caa6434144ec86ed4753d779ab94e138863
Project-Faust <pentecost.cody.lane@gmail.com>
2023-06-15 20:05:34 -0400

    updated layout/styling to center component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -40,58 +40,62 @@ export default function Contact() {
         event.preventDefault();
     }
     return (
+        // <div className="container m-3">
         <section
-            className="row container-fluid text-center m-3">
-            <h1
-            >Contact Me</h1>
-            <form
-                className="col-md-6 d-flex justify-content-center card container justify-content-center m-3">
-                <div className="m-3">
-                    <label htmlFor="name">Name</label>
-                    <input
-                        type="text"
-                        name="Name"
-                        defaultValue={name}
-                        onBlur={handleChange}
-                        placeholder="Enter Your Name"
-                        className="form-control"
-                    />
-                </div>
-                <div className="m-3">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        name="Email"
-                        defaultValue={email}
-                        onBlur={handleChange}
-                        placeholder="Enter Your Email"
-                        className="form-control"
-                    />
-                </div>
-                <div className="m-3">
-                    <label htmlFor="message">Message</label>
-                    <textarea
-                        name="Message"
-                        rows="5"
-                        defaultValue={message}
-                        onBlur={handleChange}
-                        placeholder="Enter Your Message"
-                        className="form-control"
-                    />
-                </div>
-                {errorMessage && (
-                    <div>
-                        <p className="error-text">{errorMessage}</p>
+            className="row m-3">
+            <div id="contactContainer" className="col-md-6 mx-auto">
+                <h1 className="d-flex justify-content-center"
+                >Contact Me</h1>
+                <form
+                    className="container-fluid card m-3">
+                    <div className="m-3">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            name="Name"
+                            defaultValue={name}
+                            onBlur={handleChange}
+                            placeholder="Enter Your Name"
+                            className="form-control"
+                        />
                     </div>
-                )}
-                <button
-                    type="submit"
-                    onClick={handleSubmit}
-                    className="btn btn-dark m-3"
-                >
-                    Submit
-                </button>
-            </form>
+                    <div className="m-3">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="Email"
+                            defaultValue={email}
+                            onBlur={handleChange}
+                            placeholder="Enter Your Email"
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="m-3">
+                        <label htmlFor="message">Message</label>
+                        <textarea
+                            name="Message"
+                            rows="5"
+                            defaultValue={message}
+                            onBlur={handleChange}
+                            placeholder="Enter Your Message"
+                            className="form-control"
+                        />
+                    </div>
+                    {errorMessage && (
+                        <div>
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                    )}
+                    <button
+                        type="submit"
+                        onClick={handleSubmit}
+                        className="btn btn-dark m-3"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
         </section>
+        // </div>
     );
 }
